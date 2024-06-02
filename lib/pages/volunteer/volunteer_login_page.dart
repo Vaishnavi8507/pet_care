cfc0785f99911f10a98675d9437fc4c6fd4d84ba
Vaishnavi8507 <117642376+Vaishnavi8507@users.noreply.github.com>
2024-06-02 18:40:10 +0530

    Update volunteer_login_page.dart

--- lib/pages/volunteer/volunteer_login_page.dart
+++ lib/pages/volunteer/volunteer_login_page.dart
@@ -1,13 +1,128 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:pet_care/widgets/components/text_button.dart';
+import 'package:pet_care/widgets/components/textfield.dart';
+import 'package:provider/provider.dart';
 
-class VolunteerLoginPage extends StatelessWidget {
-  const VolunteerLoginPage({super.key});
+import '../../constants/theme/light_colors.dart';
+import '../../provider/volunteer_login_provider.dart';
+
+class VolunteerLogin extends StatelessWidget {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return   Scaffold(
-      appBar: AppBar(
-        title: Text('Volunteer Login'),
+    return Scaffold(
+      backgroundColor: LightColors.primaryDarkColor,
+      body: Padding(
+        padding: const EdgeInsets.only(top: 12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SizedBox(height: 10),
+            Image.asset(
+              'assets/images/landing.jpg',
+              height: 150,
+              width: 100,
+              // fit: BoxFit.cover,
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: LightColors.scaffoldBackgroundColor,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40),
+                    topRight: Radius.circular(40),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(25.0),
+                  child: Consumer<VolunteerLoginProvider>(
+                    builder: (context, provider, child) {
+                      return SingleChildScrollView(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            MyTextField(
+                              hintText: 'Email',
+                              obsText: false,
+                              controller: _emailController,
+                              margin: EdgeInsets.only(bottom: 20),
+                              padding: EdgeInsets.zero,
+                              prefixIcon: Icon(Icons.email_outlined),
+                              textStyle: TextStyle(color: Colors.black),
+                              fillColor: Colors.white,
+                            ),
+                            MyTextField(
+                              hintText: 'Password',
+                              obsText: !provider.isPasswordVisible,
+                              controller: _passwordController,
+                              margin: EdgeInsets.only(bottom: 20),
+                              padding: EdgeInsets.zero,
+                              focusNode: FocusNode(),
+                              prefixIcon: Icon(Icons.lock_outline),
+                              suffixIcon: IconButton(
+                                icon: Icon(
+                                  provider.isPasswordVisible
+                                      ? Icons.visibility
+                                      : Icons.visibility_off,
+                                ),
+                                onPressed: () {
+                                  provider.togglePasswordVisibility();
+                                },
+                              ),
+                              textStyle: TextStyle(color: Colors.black),
+                              fillColor: Colors.white,
+                            ),
+                            SizedBox(height: 5),
+                            CustomTextButton(
+                              onPressed: () {
+                                provider.setEmail(_emailController.text);
+                                provider.setPassword(_passwordController.text);
+                                provider.login(email: '', password: '');
+                              },
+                              text: 'Sign In',
+                              backgroundColor: LightColors.primaryDarkColor,
+                              textColor: LightColors.textColor,
+                              fontSize: 15,
+                              width: 100,
+                            ),
+                            SizedBox(height: 12),
+                            RichText(
+                              text: TextSpan(
+                                  text: 'Don\'t have an account?',
+                                  style: TextStyle(
+                                    color: LightColors.textColor,
+                                    fontSize: 16,
+                                  ),
+                                  children: <TextSpan>[
+                                    TextSpan(
+                                      text: ' ',
+                                    ),
+                                    TextSpan(
+                                        text: 'Sign Up',
+                                        style: TextStyle(
+                                          fontSize: 14,
+                                          color: Colors.grey,
+                                        ),
+                                        recognizer: TapGestureRecognizer()
+                                          ..onTap = () {
+                                            Navigator.pushNamed(
+                                                context, '/volunteerReg');
+                                          })
+                                  ]),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
