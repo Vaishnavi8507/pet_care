a09b8175dce7bc6f535197df504e6becc4a8687d
Vaishnavi8507 <117642376+Vaishnavi8507@users.noreply.github.com>
2024-06-17 22:02:28 +0530

    Update pet_sitters.dart

--- lib/pages/screens/pet_sitters.dart
+++ lib/pages/screens/pet_sitters.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:pet_care/provider/pet_sitter_provider.dart';
 import 'package:provider/provider.dart';
 
+import '../volunteer/volunteer_details_page.dart';
+
 class PetSitters extends StatefulWidget {
   const PetSitters({Key? key}) : super(key: key);
 
@@ -23,6 +25,7 @@ class _PetSittersState extends State<PetSitters> {
       value: _provider,
       child: Scaffold(
         appBar: AppBar(
+          centerTitle: true,
           title: Text('Pet Sitters'),
         ),
         body: Consumer<PetSitterProvider>(
@@ -146,7 +149,39 @@ class _PetSittersState extends State<PetSitters> {
                     itemCount: provider.volunteers.length,
                     itemBuilder: (context, index) {
                       final volunteer = provider.volunteers[index];
-                      return VolunteerCard(volunteer: volunteer);
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => VolunteerDetailsPage(
+                                volunteer: {
+                                  'profileImageUrl':
+                                      volunteer['profileImageUrl'],
+                                  'name': volunteer['name'],
+                                  'phoneNo': volunteer['phoneNo'],
+                                  'aboutMe': volunteer['aboutMe'],
+                                  'locationCity': volunteer['locationCity'],
+                                  'prefersDog': volunteer['prefersDog'],
+                                  'prefersCat': volunteer['prefersCat'],
+                                  'prefersRabbit': volunteer['prefersRabbit'],
+                                  'prefersBird': volunteer['prefersBird'],
+                                  'age': volunteer['age'],
+                                  'occupation': volunteer['occupation'],
+                                  'minPrice': volunteer['minPrice'],
+                                  'providesDogWalking':
+                                      volunteer['providesDogWalking'],
+                                  'providesHomeVisits':
+                                      volunteer['providesHomeVisits'],
+                                  'providesHouseSitting':
+                                      volunteer['providesHouseSitting'],
+                                },
+                              ),
+                            ),
+                          );
+                        },
+                        child: VolunteerCard(volunteer: volunteer),
+                      );
                     },
                   ),
                 ),
@@ -158,7 +193,7 @@ class _PetSittersState extends State<PetSitters> {
     );
   }
 
-   void _showSortOptions(BuildContext context) {
+  void _showSortOptions(BuildContext context) {
     showModalBottomSheet(
       context: context,
       builder: (context) {
@@ -186,25 +221,33 @@ class _PetSittersState extends State<PetSitters> {
     );
   }
 }
-class VolunteerCard extends StatelessWidget {
+
+class VolunteerCard extends StatefulWidget {
   final Map<String, dynamic> volunteer;
 
   const VolunteerCard({Key? key, required this.volunteer}) : super(key: key);
 
   @override
+  _VolunteerCardState createState() => _VolunteerCardState();
+}
+
+class _VolunteerCardState extends State<VolunteerCard> {
+  bool _isLiked = false; // Track whether the volunteer is liked or not
+
+  @override
   Widget build(BuildContext context) {
     return Card(
-      color: Colors.white.withOpacity(0.8),
+      color: Colors.white,
       margin: EdgeInsets.symmetric(vertical: 10),
       child: Padding(
-        padding: EdgeInsets.all(15),
+        padding: EdgeInsets.all(16),
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             CircleAvatar(
               radius: 30,
-              backgroundImage: volunteer['profileImageUrl'] != null
-                  ? NetworkImage(volunteer['profileImageUrl'])
+              backgroundImage: widget.volunteer['profileImageUrl'] != null
+                  ? NetworkImage(widget.volunteer['profileImageUrl'])
                   : AssetImage('assets/images/default_profile.png')
                       as ImageProvider,
             ),
@@ -214,7 +257,7 @@ class VolunteerCard extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    volunteer['name'],
+                    widget.volunteer['name'],
                     style: TextStyle(
                       color: Colors.black,
                       fontSize: 18,
@@ -227,7 +270,7 @@ class VolunteerCard extends StatelessWidget {
                       Icon(Icons.location_on, color: Colors.grey, size: 20),
                       SizedBox(width: 5),
                       Text(
-                        volunteer['locationCity'] ?? 'Unknown',
+                        widget.volunteer['locationCity'] ?? 'Unknown',
                         style: TextStyle(
                           fontSize: 14,
                           color: Colors.grey,
@@ -238,51 +281,51 @@ class VolunteerCard extends StatelessWidget {
                   SizedBox(height: 5),
                   Row(
                     children: [
-                      if (volunteer['prefersDog'] == true)
+                      if (widget.volunteer['prefersDog'] == true)
                         Container(
                           width: 20,
                           height: 20,
                           child: Image.asset('assets/icons/dog.png'),
                         ),
-                      if (volunteer['prefersCat'] == true)
+                      if (widget.volunteer['prefersCat'] == true)
                         Container(
                           width: 20,
                           height: 20,
                           child: Image.asset('assets/icons/cat.png'),
                         ),
-                      if (volunteer['prefersRabbit'] == true)
+                      if (widget.volunteer['prefersRabbit'] == true)
                         Container(
                           width: 20,
                           height: 20,
                           child: Image.asset('assets/icons/rabbit.png'),
                         ),
-                      if (volunteer['prefersBird'] == true)
+                      if (widget.volunteer['prefersBird'] == true)
                         Container(
                           width: 20,
                           height: 20,
                           child: Image.asset('assets/icons/bird.png'),
                         ),
-                      if (volunteer['prefersDog'] == true ||
-                          volunteer['prefersCat'] == true ||
-                          volunteer['prefersRabbit'] == true ||
-                          volunteer['prefersBird'] == true)
+                      if (widget.volunteer['prefersDog'] == true ||
+                          widget.volunteer['prefersCat'] == true ||
+                          widget.volunteer['prefersRabbit'] == true ||
+                          widget.volunteer['prefersBird'] == true)
                         Text(
                           '|',
                           style: TextStyle(fontSize: 20, color: Colors.grey),
                         ),
-                      if (volunteer['providesDogWalking'] == true)
+                      if (widget.volunteer['providesDogWalking'] == true)
                         Container(
                           width: 20,
                           height: 20,
                           child: Image.asset('assets/icons/pet_walk.png'),
                         ),
-                      if (volunteer['providesHomeVisits'] == true)
+                      if (widget.volunteer['providesHomeVisits'] == true)
                         Container(
                           width: 20,
                           height: 20,
                           child: Image.asset('assets/icons/pet_home.png'),
                         ),
-                      if (volunteer['providesHouseSitting'] == true)
+                      if (widget.volunteer['providesHouseSitting'] == true)
                         Container(
                           width: 20,
                           height: 20,
@@ -299,13 +342,30 @@ class VolunteerCard extends StatelessWidget {
                   'From',
                   style: TextStyle(fontSize: 14, color: Colors.grey),
                 ),
-                Text(
-                  '${volunteer['minPrice'] ?? 0} INR',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.blue.shade500,
-                  ),
+                Row(
+                  children: [
+                    Text(
+                      '${widget.volunteer['minPrice'] ?? 0} INR',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    SizedBox(width: 5),
+                    IconButton(
+                      icon: _isLiked
+                          ? Icon(Icons.favorite)
+                          : Icon(Icons.favorite_border),
+                      color: Colors.red,
+                      onPressed: () {
+                        setState(() {
+                          _isLiked = !_isLiked; // Toggle liked state
+                        });
+                       
+                      },
+                    ),
+                  ],
                 ),
               ],
             ),
