2117e1a422685a447df5676cbae4336d67b80828
Vaishnavi8507 <117642376+Vaishnavi8507@users.noreply.github.com>
2024-06-17 22:04:54 +0530

    Update tooltips.dart

--- lib/widgets/components/tooltips.dart
+++ lib/widgets/components/tooltips.dart
@@ -1,23 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:pet_care/constants/theme/light_colors.dart';
+
 class CustomToolTip extends StatelessWidget {
-  final String? message;
-  final String? additionalInfo;
+  final String message;
+  final VoidCallback? onPressed;
 
   const CustomToolTip({
     Key? key,
     required this.message,
-    required this.additionalInfo,
+    this.onPressed,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Tooltip(
-      message: message!,
+      message: message,
       child: IconButton(
-        icon: Icon(Icons.info_outlined),
-        onPressed: () {
-          // Handle onPressed action if needed
-        },
+        icon: Icon(
+          Icons.info_outlined,
+          color: LightColors.textColor,
+        ),
+        onPressed: onPressed,
       ),
     );
   }
